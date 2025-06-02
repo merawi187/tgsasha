4c485463f4121ee0bccf8709a3d19cadcfec79f0
merawi187 <165668237+merawi187@users.noreply.github.com>
2025-06-03 00:15:48 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,21 @@
+import logging
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+# Получаем токен из переменных окружения
+TOKEN = os.environ.get('TOKEN')
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Привет')
+
+if __name__ == '__main__':
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler('start', start))
+    print('Бот запущен. Нажмите Ctrl+C для остановки.')
+    app.run_polling() 
